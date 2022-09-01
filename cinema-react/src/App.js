040752fdb4eede89719688c7090bb0033ecfa899
Marclemer <marc-lemer@hotmail.com>
2022-09-01 13:36:15 +0200

    reboot

--- cinema-react/src/App.js
+++ cinema-react/src/App.js
@@ -1,18 +1,9 @@
 
-import './App.scss';
-import NavBar from './containers/nav-Bar/nav-bar';
-import ProfilNav from './containers/profil-Nav/profil-nav';
 
+import './App.scss';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar>
-          <ProfilNav/>
-        </NavBar>
-      </header>
-    </div>
-  );
+
+  
 }
 
 export default App;
