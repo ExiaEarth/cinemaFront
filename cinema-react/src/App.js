60b2581bbfa0a7874eac13f904cc257303a8c5fd
Marclemer <marc-lemer@hotmail.com>
2022-09-01 11:55:56 +0200

    maj3

--- cinema-react/src/App.js
+++ cinema-react/src/App.js
@@ -1,11 +1,15 @@
 
-import './App.css';
+import './App.scss';
+import NavBar from './containers/nav-Bar/nav-bar';
+import ProfilNav from './containers/profil-Nav/profil-nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        ici la
+        <NavBar>
+          <ProfilNav/>
+        </NavBar>
       </header>
     </div>
   );
--- cinema-react/src/containers/nav-Bar/nav-bar.jsx
+++ cinema-react/src/containers/nav-Bar/nav-bar.jsx
@@ -1,13 +1,34 @@
+import Style from "./nav-bar.module.scss";
+import {Stack,ListItemButton, ListItemIcon, ListItemText} from "@mui/material"
+import { NavLink,Link } from "react-router-dom";
+
+// const NavBarMenu =({to,text,icon,onSelected})=>{
+//     return(
+//         <ListItemButton component={NavLink}to={to}onClick={onSelected}>
+//             <ListItemIcon>
+//                 {icon}
+//             </ListItemIcon>
+
+//             <ListItemText>
+//                 {text}
+//             </ListItemText>
+//         </ListItemButton>
+//     )
+// }
 
 
-
-
-const navBar=()=>{
+const NavBar=({onSelected})=>{
     return (
-        <nav>
-            <Stack>
-                <NavLink to="NON fonctionelle">ERROR</NavLink>
-            </Stack>
+        <nav className={Style.navContainer}>
+            <ul>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+            </ul>
         </nav>
     )
-}
+};
+
+export default NavBar;
